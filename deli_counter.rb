cd44ab4b8029f41368a3180e662790f5a428d391
Levi Borland <borland.levi@gmail.com>
2019-08-16 04:17:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,6 @@ def line(array)
     puts "The line is currently empty."
   else
     array.each_with_index do |name, index|
-      puts ""
+      puts "The line is currently: " + "#{index + 1}. " + "#{name} "
     end
 end
